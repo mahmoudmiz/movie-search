3295411ddac60173fa14e5c979ea5838890fa263
mahmoudmiz <mmahmoudmiz@holidayinnclub.com>
2024-06-26 12:15:54 -0300

    feat: simple header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { Typography } from "@mui/material";
 import MovieSearch from "./components/MovieSearch/MovieSearch";
 
 function App() {
   return (
     <div className="App">
+      <Typography  padding={2} align="center" variant="h4">Movie Search App</Typography>
       <MovieSearch/>
     </div>
   );
